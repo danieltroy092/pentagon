d94b61e853485bc79d83c05ab64e39e7feedc1e0
Daniel Troy Ong <danieltroy092@gmail.com>
2020-07-11 14:41:14 +0100

    updated testimonial section

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,17 @@
-$(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    dots: false,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      500: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
+$('#testimonial-carousel').owlCarousel({
+  autoplay: true,
+  dots: false,
+  nav: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
     },
-  });
+    700: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+    },
+  },
 });
